4761331f0d5cf52cb9082be102d0c8f71af9aae9
jose <jose@practica.com>
2025-11-03 23:23:12 +0100

    Merge branch 'main' of https://github.com/Revan115x/ProgramacionJava.git into main

---
+++ SentenciasDeControl/src/Bucles/practica2.java
@@ -0,0 +1,37 @@
+package Bucles;
+
+import java.util.Scanner;
+
+public class practica2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// TODO Auto-generated method stub
+
+				/*
+				 * Leer un número e indicar si es positivo o negativo. El proceso se repetirá
+				 * hasta que se introduzca un 0.
+				 */
+
+				try (Scanner sc = new Scanner(System.in)) {
+					int num;
+
+					do {
+						System.out.println("di un numero");
+						num = sc.nextInt();
+
+						if (num > 0)
+							System.out.println("es positivo");
+						else
+							System.out.println("es negativo");
+
+					} while (num != 0);
+				}
+
+				System.out.println("Fin del programa");
+
+
+	}
+
+}
