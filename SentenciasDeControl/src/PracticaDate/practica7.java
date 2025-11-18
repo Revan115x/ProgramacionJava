69e08f54c95a894694acb85920c6ac91a415efe6
Revan115x <217095250+Revan115x@users.noreply.github.com>
2025-11-18 23:29:23 +0100

    Añadido a practicaDate los ejercicios 6,7(sin terminar)

---
+++ SentenciasDeControl/src/PracticaDate/practica7.java
@@ -0,0 +1,40 @@
+package PracticaDate;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class practica7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/* Lee la fecha de nacimiento de una persona y calcula su edad. */
+
+		String nacimiento;
+		LocalDate dia = null;
+		boolean correcto = true;
+		DateTimeFormatter formato;
+
+		formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+		Scanner sc = new Scanner(System.in);
+
+		do {
+			System.out.println("Fecha nacimiento Ej 20/03/2000");
+			nacimiento = sc.nextLine();
+
+			try {
+				dia = LocalDate.parse(nacimiento, formato);
+				correcto = true;
+			} catch (DateTimeException e) {
+				System.out.println("error con el formato");
+				correcto = false;
+			}
+
+		} while (!correcto);
+
+	}
+
+}
