5e7e9c6f68631c47ff633a8e9845c2865d2177ed
Revan115x <217095250+Revan115x@users.noreply.github.com>
2025-11-18 14:25:11 +0100

    practicas fechas se han agregado ejercicio 4,5

--- SentenciasDeControl/src/Examenes/ExamenStrings.java
+++ SentenciasDeControl/src/Examenes/ExamenStrings.java
@@ -28,6 +28,9 @@ public class ExamenStrings {
 			while (!validarCodigo(codigo)) {
 				System.out.println("Introduce codigo articulo");
 				codigo = sc.nextLine();
+				codigo.toUpperCase();
+				
+				System.out.println(codigo);
 			}
 
 			unidades = r.nextInt(4) + 1;
@@ -40,24 +43,48 @@ public class ExamenStrings {
 			} while(precioUnidad <= 0);
 			
 			double precioproducto=CalcularPrecioTotal(codigo,unidades,precioUnidad);
+			
 			if(totalcomprausuario+precioproducto>presupuesto)
 				System.out.println("Presupuesto superado retira articulo");
 			else
 				totalcomprausuario+=precioproducto;
-				
+			
+			System.out.println("Introduce codigo articulo");
+			codigo = sc.nextLine();
 		}
+		
+		/*Gestionar pedido*/
+		
+		System.out.println("Quieres hacer pedido a domicilio?");
+		boolean pedido=sc.nextBoolean();
+		if(pedido) {
+			//preguntar si es jubilado y la zona
+			//incrementar la compra total del usuario con lo que vale hacer el envio
+		}
+		
 	}
 
-
 	public static boolean validarCodigo(String codigo) {
+		
+		/*if(codigo.length()<=5 && codigo.length()<=4 ) {
+		
+		}else
+			return false;*/
+		
 		return true;
 	}
 
 	public static double CalcularPrecioTotal(String codigo,int unidades,double precioUnidad) {
 		double importe=0;
-		
-		if(codigo.charAt(1)==L)
-		
+		/*
+		if (codigo.charAt(1)=='L')
+			
+		else if (codigo.charAt(1)=='P')
+			
+		else
+		*/
+			
+		return importe;
 		
 	}
 
