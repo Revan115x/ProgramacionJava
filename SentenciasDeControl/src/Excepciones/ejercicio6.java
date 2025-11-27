700c820c064edde21ddbb697f16cf9fe6ea83cc2
Revan115x <217095250+Revan115x@users.noreply.github.com>
2025-11-27 11:14:51 +0100

    Añadido nuevo ejercicio6 Excepciones(Proceso)

---
+++ SentenciasDeControl/src/Excepciones/ejercicio6.java
@@ -0,0 +1,16 @@
+package Excepciones;
+
+public class ejercicio6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * 2. Lee dos cadenas. Haz una función que devuelva el número de veces que la
+		 * primera está incluida en la segunda. Devuelve una excepción propia si
+		 * cualquiera de las dos está vacía.
+		 */
+
+	}
+
+}
