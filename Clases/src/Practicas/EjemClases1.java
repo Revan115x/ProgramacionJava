551ee91f3788805d2735a3c1804c960acc62e3ca
Revan115x <217095250+Revan115x@users.noreply.github.com>
2026-01-15 10:08:16 +0100

    Clases

---
+++ Clases/src/Practicas/EjemClases1.java
@@ -0,0 +1,50 @@
+
+package Practicas;
+import java.util.*;
+/**
+ *
+ * @author rcerrato
+ */
+public class EjemClases1 {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Persona miPersona1, miPersona2;
+    Scanner sc=new Scanner(System.in);
+    miPersona1=new Persona('H');
+    miPersona2=new Persona('M',"22222B",43,3,false);
+    //Pedir datos de persona por teclado
+    Persona miPersona3;
+    System.out.print("Introduce NIF: ");
+    String nif=sc.nextLine();
+    System.out.print("Introduce tu edad: ");
+    int e=sc.nextInt();
+    System.out.append("¿Cuantos hijos tienes? ");
+    int nh=sc.nextInt();
+    System.out.print("¿Estás casado?(C/S)");
+    boolean C=sc.nextBoolean();
+    sc.nextLine();
+    System.out.print("Introduce sexo(H/M)" );
+    char s=sc.nextLine().charAt(0);
+    miPersona3=new Persona(s,nif,e,nh,C);
+    
+    System.out.println("La edad de la primera persona es:"+miPersona1.getEdad());
+    if (miPersona3.casarse()==false)
+        System.out.println("¡QUE YA ESTÁS CASADOOO!!!");
+    else
+        System.out.println("¡ENHORABUENAAA!!!!");
+    miPersona3.tenerHijos(2);
+    miPersona1.cumplirAnyos();
+    System.out.println(miPersona1.toString());
+    System.out.println(miPersona2.toString());
+    System.out.println(miPersona3.toString());
+    //ABAJO: SI FUERA PUBLICO
+    //System.out.println("Número total de personas: "+Persona.numeroPersonas); 
+    System.out.println("Número total de personas: "+Persona.getNumeroPersonas());
+    System.out.println("Se han celebrado: "+Persona.getNumBodas()+" bodas");
+    //SI PIDE ALGO GLOBAL SERA UN METODO ESTATICO
+    }
+    
+    
+    
+}
---
+++ Clases/src/Practicas/Persona.java
@@ -0,0 +1,97 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practicas;
+
+
+public class Persona {
+    private String nif;
+    private int edad,numHijos;
+    private char sexo;
+    private boolean casado;
+    private static int numeroPersonas,numBodas; //tienen acceso todos los objetos pero solo se usa una vez
+    
+    
+    public Persona() {
+        numeroPersonas++;
+    }
+    public Persona(char s){
+        nif="";
+        edad=0;
+        numHijos=0;
+        casado=false;
+        sexo=s;
+        numeroPersonas++;
+    }
+    
+    public Persona(String nif, int edad, int numHijos, char sexo, boolean casado) {
+        this.nif = nif;
+        this.edad = edad;
+        this.numHijos = numHijos;
+        this.sexo = sexo;
+        this.casado = casado;
+        numeroPersonas++;
+    }
+    
+    //Aqui se usa this, porque al poner entre patentesis el mismo nombre que las variables
+    //necesitas especificar cual es el que quieres como parametro y dato
+
+    public Persona(char s, String n, int e, int nh, boolean c){
+      nif=n;
+      edad=e;
+      numHijos=nh;
+      sexo=s;
+      casado=c;
+      numeroPersonas++;
+    }
+    
+    public void cumplirAnyos(){
+        edad++;
+    }
+    
+    public boolean casarse(){
+        if (casado)
+            return false;
+        else{
+            casado=true;
+            numBodas++;
+            return true;
+        }
+    }
+            
+    public void tenerHijos(int nh){
+        numHijos=numHijos+nh;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nif=" + nif + ", edad=" + edad + ", numHijos=" + numHijos + ", sexo=" + sexo + ", casado=" + casado + '}';
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+    public static int getNumeroPersonas() {
+        return numeroPersonas;
+    }
+    public static void setNumeroPersonas(int numeroPersonas) {
+        Persona.numeroPersonas = numeroPersonas;
+    }
+    //UN METODO QUE SOLO USA VARIABLES ESTATICAS SOLO USA METODOS ESTATICOS
+    public static int getNumBodas() {
+        return numBodas;
+    }
+    public static void setNumBodas(int numBodas) {
+        Persona.numBodas = numBodas;
+    }
+    
+}
