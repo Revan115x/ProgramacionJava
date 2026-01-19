a49d2d48ac5f8e811ce026da13265814e6f77f0b
Revan115x <217095250+Revan115x@users.noreply.github.com>
2026-01-19 14:38:18 +0100

    EjercioALumnos

--- Clases y Objetos/src/MediaAlumnos/MediaAlum.java
+++ Clases y Objetos/src/MediaAlumnos/MediaAlum.java
@@ -1,5 +1,70 @@
 package MediaAlumnos;
 
+/*Al ejercicio número 3 añadirle una variable que guarde el número de alumnos con
+nota media aprobada.*/
+
 public class MediaAlum {
 
+	private static int numalum;
+	private double notaJava;
+	private double bbddd;
+	private double marcas;
+	private static double media;
+	private static int contalum;
+	private static double mediageneral;
+	
+	public MediaAlum(int numalum, double j, double b, double m ) {
+		super();
+		this.numalum = numalum;
+		this.notaJava = j;
+		this.bbddd = b;
+		this.marcas = m;
+		this.media= m+j+b/3;
+		this.mediageneral=media/contalum;
+	}
+	
+	public int getNumalum() {
+		return numalum;
+	}
+	public void setNumalum(int numalum) {
+		this.numalum = numalum;
+		contalum++;
+	}
+	public double getNotaJava() {
+		return notaJava;
+	}
+	public void setNotaJava(double notaJava) {
+		this.notaJava = notaJava;
+	}
+	public double getBbddd() {
+		return bbddd;
+	}
+	public void setBbddd(double bbddd) {
+		this.bbddd = bbddd;
+	}
+	public double getMarcas() {
+		return marcas;
+	}
+	public void setMarcas(double marcas) {
+		this.marcas = marcas;
+	}
+	public static double getMedia() {
+		return media;
+	}
+	public static void getMedia(double media) {
+		MediaAlum.media = media;
+	}
+	
+	public static void getmediaclase() {
+		MediaAlum.getMedia();;
+	}
+
+	@Override
+	public String toString() {
+		return "MediaAlum [numalum=" + numalum + ", notaJava=" + notaJava + ", bbddd=" + bbddd + ", marcas=" + marcas
+				+ "]";
+	}
+	
+	
+	
 }
