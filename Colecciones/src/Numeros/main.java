1ad8d32ade5f88358dc26c824a0a85f2aa0839d8
Revan115x <217095250+Revan115x@users.noreply.github.com>
2026-02-16 14:36:51 +0100

    Nuevos Ejercicios colecciones

---
+++ Colecciones/src/Numeros/main.java
@@ -0,0 +1,59 @@
+package Numeros;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Lee un conjunto de números, terminando al anotar cero. Lee otro conjunto de
+		 * números, termina también al anotar cero. Muestra el contenido de los dos
+		 * conjuntos. Di si un conjunto está incluido en otro. Usa HashSet.
+		 */
+
+		Scanner sc = new Scanner(System.in);
+		HashSet<Integer> Numero = new HashSet<>();
+		HashSet<Integer> Numero2 = new HashSet<>();
+
+		int num;
+
+		System.out.println("********************************************");
+		System.out.println("****************CONJUNTO 1******************");
+		System.out.println("********************************************");
+
+		while (true) {
+			System.out.println("Introduce un numero (0 para terminar): ");
+			num = sc.nextInt();
+
+			if (num == 0)
+				break;
+
+			Numero.add(num);
+			System.out.println("NUMERO AÑADIDO");
+		}
+
+		System.out.println("********************************************");
+		System.out.println("****************CONJUNTO 2******************");
+		System.out.println("********************************************");
+
+		while (true) {
+			System.out.println("Introduce un numero (0 para terminar): ");
+			num = sc.nextInt();
+
+			if (num == 0)
+				break;
+
+			Numero2.add(num);
+			System.out.println("NUMERO AÑADIDO");
+
+		}
+
+		if (Numero.containsAll(Numero2)) {
+			System.out.println(Numero2 + "Esta incluido" + Numero);
+		} else {
+			System.out.println(Numero + "Esta incluido" + Numero2);
+		}
+	}
+}
---
+++ Colecciones/src/vehículosAlquiler/Vehiculo.java
@@ -0,0 +1,80 @@
+package vehículosAlquiler;
+
+public class Vehiculo {
+
+	private String matricula, modelo, marca;
+	private int año, categoria;
+	private boolean alquilado;
+	private double ganancias;
+	private static int totalAlquilados;
+	private static double totalGanancias;
+	
+	public Vehiculo(String matricula, String modelo, String marca, int año, int categoria) {
+		this.matricula = matricula;
+		this.modelo = modelo;
+		this.marca = marca;
+		this.año = año;
+		this.categoria = categoria;
+	}
+	
+	public double alquilar(int dias) {
+		double precioDia=0;
+		double precioAlquiler;
+		
+		if (this.alquilado)
+			return 0;
+		if (this.categoria==1)
+			if (dias<=2)
+				precioDia=45;
+			else
+				precioDia=30;
+		else
+			if (dias<=2)
+				precioDia=50;
+			else
+				precioDia=40;
+		
+		precioAlquiler=precioDia*dias;
+		this.alquilado=true;
+		this.ganancias+=precioAlquiler;
+		totalGanancias+=precioAlquiler;
+		totalAlquilados++;
+		return precioAlquiler;
+					
+	}
+	
+	
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public boolean devolver() {
+		if (!alquilado)
+			return false;
+		alquilado=false;
+		totalAlquilados--;
+		return true;
+	}
+
+	public boolean isAlquilado() {
+		return alquilado;
+	}
+
+	public double getGanancias() {
+		return ganancias;
+	}
+
+	public static double getTotalGanancias() {
+		return totalGanancias;
+	}
+
+	@Override
+	public String toString() {
+		return "Vehiculo [matricula=" + matricula + ", modelo=" + modelo + ", marca=" + marca + ", año=" + año
+				+ ", categoria=" + categoria + ", alquilado=" + alquilado + ", ganancias=" + ganancias + "]";
+	}
+
+
+	
+
+}
