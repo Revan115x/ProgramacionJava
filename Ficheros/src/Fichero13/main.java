1a152aadee3d9a888948886c6b48eaceb783b295
jose <jose@practica.com>
2026-03-09 00:00:49 +0100

    cosas

--- Ficheros/src/Fichero13/main.java
+++ Ficheros/src/Fichero13/main.java
@@ -25,27 +25,27 @@ public class main {
 		 * alumnos. En cada línea separada por comas, aparece el nombre, primer
 		 * apellido, nota (double) y fecha de nacimiento de cada alumno. Con los alumnos
 		 * leídos crea un ArrayList, luego lo ordena por nota de mayor a menor.controlar
-		 * excepciones los errores de nota y fechas y guardarlos en un fichero log, corregir
-		 * el fichero y mostrar los errores log por pantalla.
+		 * excepciones los errores de nota y fechas y guardarlos en un fichero log,
+		 * corregir el fichero y mostrar los errores log por pantalla.
 		 */
 
 		Scanner sc = new Scanner(System.in);
-		
-		DateTimeFormatter d=DateTimeFormatter.ofPattern("dd/LL/yyyy");
+
+		DateTimeFormatter d = DateTimeFormatter.ofPattern("dd/LL/yyyy");
 
 		/* Declaro el tipo de codificacion */
 		Charset charset = Charset.forName("UTF-8");
 
 		/* Declaro la direccion del fichero */
 		Path AlumFich = Paths.get("Alumno.txt");
-		
-		/* Declaro en fichero para guardar errores*/
+
+		/* Declaro en fichero para guardar errores */
 		Path errores = Paths.get("log.txt");
 
 		/* Declaro buffer para poder leer el fichero */
 		BufferedReader leer = null;
-		
-		/*Declaro buffer para escribir los errores el fichero log.txt*/
+
+		/* Declaro buffer para escribir los errores el fichero log.txt */
 		BufferedWriter escribir = null;
 
 		/* Declaro arraylist para guardar los datos del fichero */
@@ -58,69 +58,68 @@ public class main {
 			try {
 				/* Leer es fichero y la codificacion */
 				leer = Files.newBufferedReader(AlumFich, charset);
-				escribir=Files.newBufferedWriter(errores, charset);
-				
+				escribir = Files.newBufferedWriter(errores, charset);
+
 				String line = null;
 
 				while ((line = leer.readLine()) != null) {
-
-					/* Creamos un array de String */
-					/* separamos los datos del fichero con split */
-					String separacion[] = line.split(",");
-
-					/* guardamos los datos en string */
-					String nombre = separacion[0];
-					String apellido = separacion[1];
-					Double nota = Double.parseDouble(separacion[2]);
-					LocalDate fecha = LocalDate.parse(separacion[3],d);
-			
-					/*guardo el objeto en la clase*/
-					Alumno a = new Alumno(nombre, apellido, nota, fecha);
 					
-					/*llamo mi funcion pos para ordenar*/
-					int posicion=pos(Alumnos,a);
-					
-					Alumnos.add(posicion,a);
+					try {
+						/* Creamos un array de String */
+						/* separamos los datos del fichero con split */
+						String separacion[] = line.split(",");
+
+						/* guardamos los datos en string */
+						String nombre = separacion[0];
+						String apellido = separacion[1];
+						Alumno a;
+						
+						Double nota = Double.parseDouble(separacion[2]);
+						LocalDate fecha = LocalDate.parse(separacion[3], d);
+						/* guardo el objeto en la clase */
+						a = new Alumno(nombre, apellido, nota, fecha);
+						/* llamo mi funcion pos para ordenar */
+						int posicion = pos(Alumnos, a);
+
+						Alumnos.add(posicion, a);
+					}catch (DateTimeParseException e) {
+						escribir.write("ERROR EN : "+line);
+					}catch (NumberFormatException e) {
+						escribir.write("ERROR EN : "+line);
+					}
 
 				}
-				
+
 				for (Alumno al : Alumnos) {
-				    System.out.println(al);
-				    
+					System.out.println(al);
+
 				}
 
 			} catch (IOException e) {
 				e.printStackTrace();
-			}catch (DateTimeParseException e) {
-				System.out.println("Error en formato fecha");
-				escribir.write(line);
-			}catch (NumberFormatException e) {
-				System.out.println("ERROR EN LA NOTA");
 			}
-			System.out.println("Fin del programa");
 			
+			System.out.println("Fin del programa");
+
 		}
 
 	}
 
-	/*Funcion que devuelve la posicion*/
+	/* Funcion que devuelve la posicion */
 	public static int pos(LinkedList<Alumno> alum, Alumno a) {
-		
-		
-		/*Version con iterator*/
-		/*int cont=0;
-		
-		Iterator<Alumno> it=alum.iterator();
-		while(it.hasNext()) {
-			if(it.next().getNota()<a.getNota())
-				return cont;
-			cont++;
-		}*/
-		
-		/*leermos la clase*/
-		for (int i=0;i<alum.size();i++) {
-			/*si nota es mayor que el alumno i devuelve la posicion i*/
-			if(a.getNota()>alum.get(i).getNota())
+
+		/* Version con iterator */
+		/*
+		 * int cont=0;
+		 * 
+		 * Iterator<Alumno> it=alum.iterator(); while(it.hasNext()) {
+		 * if(it.next().getNota()<a.getNota()) return cont; cont++; }
+		 */
+
+		/* leermos la clase */
+		for (int i = 0; i < alum.size(); i++) {
+			/* si nota es mayor que el alumno i devuelve la posicion i */
+			if (a.getNota() > alum.get(i).getNota())
 				return i;
 		}
 		return alum.size();
