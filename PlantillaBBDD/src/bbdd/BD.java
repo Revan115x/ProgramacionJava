8e8aa2d0763a15ec7770a9be7dada4b3cdc0b1a3
Revan115x <217095250+Revan115x@users.noreply.github.com>
2026-05-05 09:27:04 +0200

    BBDD Y MAS

---
+++ PlantillaBBDD/src/bbdd/BD.java
@@ -0,0 +1,20 @@
+package bbdd;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import java.time.*;
+
+
+public class BD extends BD_Conector{
+
+	private static Statement s;	
+	private static ResultSet reg;
+	
+	public BD(String file){
+		super(file);
+	}
+	
+	
+
+}
